70c8761f4eb6d1c70e794d8cc94b62febd15853e
SangEun Lee <d4space@gmail.com>
2019-07-02 19:23:03 +0200

    higgs mas histo for lhe edm format

--- LHEInterface/test/runCmsDriver.sh
+++ LHEInterface/test/runCmsDriver.sh
@@ -1,2 +1,3 @@
+cmsDriver.py MCDBtoEDM --conditions auto:mc -s NONE  --eventcontent RAWSIM --datatier GEN  --filein file:/eos/user/s/salee/HWWTo2L2Nu_NNLOPS/LHEfiles/cmsgrid_final_9_JHU.lhe --no_exec -n 100
 #cmsDriver.py MCDBtoEDM_ggHZ_Vlept --conditions auto:mc -s NONE  --eventcontent RAWSIM --datatier GEN  --filein file:ggHZ_HanythingJ_NNPDF31_13TeV_M125_Vleptonic/cmsgrid_final1.lhe --no_exec -n -1
-cmsDriver.py MCDBtoEDM_WZmll0001 --conditions auto:mc -s NONE  --eventcontent RAWSIM --datatier GEN  --filein file:../../../genproductions/bin/Powheg/cmsgrid_final1.lhe --no_exec -n -1
+#cmsDriver.py MCDBtoEDM_WZmll0001 --conditions auto:mc -s NONE  --eventcontent RAWSIM --datatier GEN  --filein file:../../../genproductions/bin/Powheg/cmsgrid_final1.lhe --no_exec -n -1
