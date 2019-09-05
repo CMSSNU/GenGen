7614b7595812ba42b492f1d6676b8a8d698c2498
SangEun Lee <d4space@gmail.com>
2019-09-05 08:16:30 +0200

    ha

---
+++ LHEInterface/test/runCmsDriver.sh
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+cmsDriver.py MCDBtoEDM --conditions auto:mc -s NONE  --eventcontent RAWSIM --datatier GEN  --filein file:/eos/user/s/salee/HWWTo2L2Nu_NNLOPS/LHEfiles/cmsgrid_final_9_JHU.lhe --no_exec -n 1
+#cmsDriver.py MCDBtoEDM_WZmll0001 --conditions auto:mc -s NONE  --eventcontent RAWSIM --datatier GEN  --filein file:../../../genproductions/bin/Powheg/cmsgrid_final1.lhe --no_exec -n -1
+#cmsDriver.py MCDBtoEDM_ggHZ_Vlept --conditions auto:mc -s NONE  --eventcontent RAWSIM --datatier GEN  --filein file:ggHZ_HanythingJ_NNPDF31_13TeV_M125_Vleptonic/cmsgrid_final1.lhe --no_exec -n -1
+=======
+cmsDriver.py MCDBtoEDM --conditions auto:mc -s NONE  --eventcontent RAWSIM --datatier GEN  --filein file:/eos/user/s/salee/HWWTo2L2Nu_NNLOPS/LHEfiles/cmsgrid_final_9_JHU.lhe --no_exec -n 100
+#cmsDriver.py MCDBtoEDM_ggHZ_Vlept --conditions auto:mc -s NONE  --eventcontent RAWSIM --datatier GEN  --filein file:ggHZ_HanythingJ_NNPDF31_13TeV_M125_Vleptonic/cmsgrid_final1.lhe --no_exec -n -1
+#cmsDriver.py MCDBtoEDM_WZmll0001 --conditions auto:mc -s NONE  --eventcontent RAWSIM --datatier GEN  --filein file:../../../genproductions/bin/Powheg/cmsgrid_final1.lhe --no_exec -n -1
+>>>>>>> 70c8761f4eb6d1c70e794d8cc94b62febd15853e
